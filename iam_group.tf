3e872f8ed3fdd4928449ac9024bcb3ce15011442
pornima <pornimasarang@gmai.com>
2023-05-12 20:41:00 +0530

    my1st group

--- iam_group.tf
+++ iam_group.tf
@@ -1,8 +1,32 @@
 provider "aws" {
   
 }
-resource "aws_iam_user" "tf_users" {
-    count = 5
-    name = ["aws_iam_user1","aws_iam_user2","aws_iam_user3","aws_iam_user4","aws_iam_user5"]
+resource "aws_iam_user" "tf-user1" {
+    name = "shaista"
 
+}
+resource "aws_iam_user" "tf-user2" {
+    name = "shweta"
+
+}
+resource "aws_iam_user" "tf-user3" {
+    name = "sagar"
+
+}
+resource "aws_iam_user" "tf-user4" {
+    name = "swapnil"
+}
+resource "aws_iam_user" "tf-user5" {
+    name = "sarang"
+}
+resource "aws_iam_group" "tf-group" {
+    name = "cloudblitz"
+}
+resource "aws_iam_group_membership" "cbz_members" {
+   [user = aws_iam_user.tf-user1.shaista
+    user = aws_iam_user.tf-user2.shweta
+    user = aws_iam_user.tf-user3.sagar
+    user = aws_iam_user.tf-user4.swapnil
+    user = aws_iam_user.tf-user5.sarang]
+    groups =[aws_iam_group.tf-group.cloudblitz]        
 }
