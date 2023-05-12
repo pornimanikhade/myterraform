3bd80a506c700e91beb859b9f147fd58acf676b1
pornima <pornimasarang@gmai.com>
2023-05-12 21:50:41 +0530

    new1

---
+++ new.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  
+}
+resource "aws_iam_group_membership" "cloudblitz" {
+  name = "aws-group1"
+
+  users = [
+    aws_iam_user.pornima.tf-user1,
+    aws_iam_user.sadhika.tf-user2,
+  ]
+
+  group = aws_iam_group.tf_group.aws-group
+}
+
+resource "aws_iam_group" "tf_group" {
+  name = "aws-group"
+}
+
+resource "aws_iam_user" "pornima" {
+  name = "tf-user1"
+}
+
+resource "aws_iam_user" "sadhika" {
+  name = "tf-user2"
+}
