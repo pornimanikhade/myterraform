f4eed7b00ded92916f1ba656414273bba85aec4e
pornimanikhade <125467034+pornimanikhade@users.noreply.github.com>
2023-05-24 14:57:25 +0530

    Delete tf1 directory

--- tf1/userdata.sh
+++
@@ -1,6 +0,0 @@
-sudo yum update -y
-sudo yum install httpd -y
-sudo touch /var/www/html/index.html
-sudo echo "Welcome to my website $(hostname)" > /var/www/html/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
